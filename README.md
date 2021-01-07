00a2c5b13e1d06587737d17158f9807aeadc85cd
Kassian Wren <the@nodebotani.st>
2021-01-07 13:53:24 -0600

    Added TODOs for future features

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # standup-chatbot
+
+# TODO
+
+* Add database/plan storage
+* Finish designing the standup object
+* Figure out how to add users to a standup (do we need a Zoom user object?)
+* Figure out how to DM/message an arbitrary channel
--- src/standup.js
+++ src/standup.js
@@ -6,7 +6,11 @@ module.exports = {
     start: () => {
         let standupId = uuidv4()
         standups.push({
-            id: standupId
+            id: standupId,
+            reporters: [],
+            reports: [],
+            endTime: new Date(), //TODO: add moment to handle datetimes
+            reportChannel: {} //TODO: figure out how to send messages to other channels
         })
         return standupId
     }
