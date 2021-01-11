25c3bde2751c75d3c37e853431cf502fea276af4
Kassian Wren <the@nodebotani.st>
2021-01-11 13:36:26 -0600

    Added mongodb

--- app.js
+++ app.js
@@ -9,9 +9,25 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser')
 const dotenv = require('dotenv');
 const hbs = require('./hbs');
+const mongoClient = require('mongodb').MongoClient
 const NODE_ENV='development';
 
 dotenv.config()
+
+//connect to db
+const DB_URL = process.env.mongodb_uri + ':27017'
+const DB_NAME = "standupbot"
+
+console.log(DB_URL)
+
+mongoClient.connect(DB_URL, function(err, client) {
+  console.log(err)
+  console.log("Connected successfully to server");
+ 
+  const db = client.db(DB_NAME);
+ 
+  client.close();
+});
 
 //middleware body
 expressApp.use(bodyParser.json());
